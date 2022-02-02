2f17889a1d1e544d0571e338802f3c8cab8b127b
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2022-02-02 17:32:44 +0000

    [v0.1.1][02/02/2022] People now cannot pass community bounds

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Pathogen:
         pass 
 
 
-    def infect(self, personn, infected) -> bool:
+    def infect(self, person, infected) -> bool:
         '''
         Given two people who have been in contact;
         returns whether the infected person infects the other 'person'
@@ -50,14 +50,13 @@ class Simulation:
         app_width, app_height = self.app_size
         layout = self.sim_vars['community_layout'] 
         y_buffer = app_height/(len(layout)*10) # The pixels between each row of communities 
-        height = round((app_height - (y_buffer*(len(layout)+1))) / len(layout), 2) 
+        height = round((app_height - (y_buffer*(len(layout)+1))) / len(layout)) 
         # Create each community in grid defined by layout
         for y, cols in enumerate(layout): # y counts how many rows in 
             x_buffer = app_width/(cols*10) # The pixels between each column of communities
-            width = round((app_width - (x_buffer*(cols+1))) / cols, 2)  
+            width = round((app_width - (x_buffer*(cols+1))) / cols)  
             for x in range(cols): # x counts how many columns in 
                 coords = round((x*(width+x_buffer)+x_buffer)), round(y*(height+y_buffer)+y_buffer) 
-                print(coords)
                 communities.append(Community(coords, (width, height), self.sim_vars, self.theme))
                 
         return communities
@@ -124,9 +123,7 @@ class Community():
         self.population = pygame.sprite.Group()
 
         for person in range(self.sim_vars['community_size']):
-           x = random.uniform(1,self.surface_size[0])
-           y = random.uniform(1,self.surface_size[1])
-           self.population.add(Person((x, y), self.sim_vars, self.theme))
+          self.population.add(Person(self.surface_size, self.sim_vars, self.theme))
 
     def calculate_infected(self):
         pass
@@ -141,17 +138,20 @@ class Community():
 
 class Person(pygame.sprite.Sprite):
 
-    def __init__(self, coords, sim_vars, theme=None) -> None:
+    def __init__(self, community_size, sim_vars, theme=None) -> None:
 
         # If people will be rendered then a colour palette is required 
         if render and theme == None:
             raise AttributeError("No 'theme' attribute provided for person to use")
 
+        self.community_size = community_size
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((5,5)) # Set size of object that is rendered to window
+        self.size = (5,5)
+        self.image = pygame.Surface(self.size) # Set size of object that is rendered to window
         self.image.fill(theme['susceptible']) # Set background to colour of uninfected person
         self.rect = self.image.get_rect() # The rectangle object that controlls position of person 
-        self.rect.x, self.rect.y = coords
+        self.rect.x = random.randint(1,self.community_size[0] - self.size[0])
+        self.rect.y = random.randint(1,self.community_size[1] - self.size[1])
         self.sim_vars = sim_vars # Get simulation variables from config.json  loaded in community
         self.movement = 2  
 
@@ -168,9 +168,23 @@ class Person(pygame.sprite.Sprite):
         '''
         new_x = eval(f'{self.rect.x}{random.choice(["+", "-"])}{self.movement}')
         new_y = eval(f'{self.rect.y}{random.choice(["+", "-"])}{self.movement}')
-
+        max_x, max_y = self.community_size
+        width, height = self.size
+        valid_x, valid_y  = max_x - width, max_y - height
+        # If new x coord out of range
+        if new_x < 0: 
+            new_x = 0
+        elif new_x > valid_x:
+            new_x = valid_x
+        # If new y coord out of range
+        if new_y < 0:
+            new_y = 0 
+        elif new_y > valid_y:
+            new_y = valid_y
+        # Coordinates definitely in range
         self.rect.x = new_x
         self.rect.y = new_y
+    
    
 
 class Test:
