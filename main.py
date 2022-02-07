370f4ec8e4711b847f7d55d70f394a96d5826a98
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2022-02-07 15:33:12 +0000

    [v0.1.3][07/02/2022] Updated name spaces for different frames

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # Made for AQA A level Computer Science NEA 2021/22
 import pygame, json, os, render, random, time
 
+pygame.init()
+pygame.font.init()
+
+
 class Pathogen:
     '''
     Class that contains methods for characteristics of pathogen spread
@@ -40,10 +44,10 @@ class Simulation:
         with open(config, 'r') as config_file:
             config = json.loads(config_file.read())
 
-        self.app_size = tuple(config['app']['window_size'])
-        self.sidebar_size = tuple(config['app']['sidebar_size'])
-        
-        self.window_size = self.app_size[0] + self.sidebar_size[0], max([self.app_size[1], self.sidebar_size[1]])  
+        self.sim_size = (config['app']['sim_size'])
+        self.sidebar_size = (config['app']['sidebar_width'], self.sim_size[1])
+        self.botbar_size = (self.sim_size[0], config['app']['bar_height']) 
+        self.controls_size = (config['app']['sidebar_width'], config['app']['bar_height']) 
         self.theme = config['theme'][config['app']['theme']]
         self.sim_vars = config['simulation']    
         self.communities = self.calculate_communities()
@@ -51,15 +55,15 @@ class Simulation:
     def calculate_communities(self):
 
         communities = []
-        app_width, app_height = self.app_size
+        sim_width, sim_height = self.sim_size
         layout = self.sim_vars['community_layout'] 
-        y_buffer = app_height/(len(layout)*10) # The pixels between each row of communities 
-        height = round((app_height - (y_buffer*(len(layout)+1))) / len(layout)) 
+        y_buffer = sim_height/(len(layout)*10) # The pixels between each row of communities 
+        height = round((sim_height - (y_buffer*(len(layout)+1))) / len(layout)) 
 
         # Create each community in grid defined by layout
         for y, cols in enumerate(layout): # y counts how many rows in 
-            x_buffer = app_width/(cols*10) # The pixels between each column of communities
-            width = round((app_width - (x_buffer*(cols+1))) / cols)  
+            x_buffer = sim_width/(cols*10) # The pixels between each column of communities
+            width = round((sim_width - (x_buffer*(cols+1))) / cols)  
 
             for x in range(cols): # x counts how many columns in 
                 coords = round((x*(width+x_buffer)+x_buffer)), round(y*(height+y_buffer)+y_buffer) 
@@ -67,51 +71,65 @@ class Simulation:
                 
         return communities
 
-    def pause(self) -> None:
+    def __pause(self) -> None:
         pass
 
 
-    def speed_up(self) -> None:
+    def __speed_up(self) -> None:
         pass
 
 
-    def slow_down(self) -> None:
+    def __slow_down(self) -> None:
         pass 
 
 
-    def render_info(self) -> None:
-        pass
+    def __render_info(self) -> None:
 
+        infected_label = self.font.render(f'Infected:{10}', True, self.theme['susceptible'])
+        susceptible_label = self.font.render(f'Susceptible:{10}', True, self.theme['susceptible'])
+ 
 
-    def render_graph(self) -> None:
+    def __render_graph(self) -> None:
         pass
 
 
     def run(self) -> None:
         
         # Create application size defined by config json
-        self.window = pygame.display.set_mode(self.window_size)
+        window_size = (self.sim_size[0] + self.sidebar_size[0], self.sim_size[1] + self.botbar_size[1])
+        self.window = pygame.display.set_mode(window_size)
+        self.sim_surf= pygame.Surface(self.sim_size)
+        self.sidebar_surf = pygame.Surface(self.sidebar_size)
+        self.botbar_surf = pygame.Surface(self.botbar_size)
+        self.controls_surf = pygame.Surface(self.controls_size)
         self.running = True
         bgcolor = self.theme['app_background'] 
 
+        # Font Initialisation
+        self.font = pygame.font.SysFont('Calibri', 20)
+
         while self.running:
             
-            self.window.fill(bgcolor) # Fill background            
+            self.sim_surf.fill(bgcolor) # Fill background            
+            self.sidebar_surf.fill(bgcolor) # Fill background            
 
             # Render the canvas of each community and update
             for community in self.communities:
                 community.update()
-                self.window.blit(community.surface, community.coords)
+                self.sim_surf.blit(community.surface, community.coords)
 
             for event in pygame.event.get():
                 # User closed window -> quit application
                 if event.type == pygame.QUIT: 
                     self.running = False
 
+            self.window.blit(self.sim_surf, (0, 0))
+            self.window.blit(self.sidebar_surf, (self.sim_size[0], 0))
+            self.window.blit(self.controls_surf, (self.sim_size))
+            self.window.blit(self.botbar_surf, (0, self.sim_size[1]))
             pygame.display.update() 
             time.sleep(0.02)
              
-
 
 class Community():
     '''
