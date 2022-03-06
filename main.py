04e06f4936604c31d45011aa886917608541118e
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2022-03-06 14:34:43 +0000

    [v0.6][6/3/22] Finished basic menu system.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Made for AQA A level Computer Science NEA 2021/22
 # pylint: disable=E1101 
 import pygame, json, random, time, math
+import numpy as np
 
 # Imports for menu screen 
 import tkinter as tk 
@@ -358,13 +359,13 @@ class Simulation:
 
         # Create each community in grid defined by layout
         for y, cols in enumerate(layout): # y counts how many rows in
-            x_buffer = sim_width/(cols*10) # The pixels between each column of communities
+            x_buffer = sim_width/(len(cols)*10) # The pixels between each column of communities
             # Round to prevent floating error when dividing
-            width = round((sim_width - (x_buffer*(cols+1))) / cols)
-            for x in range(cols): # x counts how many columns in
+            width = round((sim_width - (x_buffer*(len(cols)+1))) / len(cols))
+            for x, (pop, places) in enumerate(cols): # x counts how many columns in
                 # Round do prevent float error when dividing
                 coords = round((x*(width+x_buffer)+x_buffer)), round(y*(height+self.y_buffer)+self.y_buffer)
-                communities.append(Community(coords, (width, height)))
+                communities.append(Community(coords, (width, height), pop, places))
 
         return communities
 
@@ -402,7 +403,10 @@ class Simulation:
         '''
 
         # Calculate R number
-        r = round(1 + (sim_vars['susceptible'] - sim_vars['infected']) / sim_vars['old_infected'], 2)
+        try:
+            r = round(1 + (sim_vars['susceptible'] - sim_vars['infected']) / sim_vars['old_infected'], 2)
+        except ZeroDivisionError: 
+            r = 0
         sim_vars['old_infected'] = sim_vars['infected']
 
         # Update counter on label
@@ -760,7 +764,7 @@ class Community:
     Is a pygame frame that sits encapsulated within simulation
     '''
 
-    def __init__(self, coords, surf_size) -> None:
+    def __init__(self, coords, surf_size, population, places) -> None:
 
         global sim_vars, theme, pathogen
 
@@ -770,14 +774,13 @@ class Community:
 
         # Create list of people in the community
         self.population = pygame.sprite.Group()
-        for _ in range(sim_vars['community_size']):
+        for _ in range(population):
             self.population.add(Person(self.surf_size))
 
         # Create places in community 
-        num_places = random.randint(sim_vars['place_lower_bound'], sim_vars['place_upper_bound'])
         self.places = pygame.sprite.Group()
 
-        for place in range(num_places):
+        for place in range(places):
             self.places.add(Place(self.surf_size))
 
     def update(self):
@@ -812,7 +815,7 @@ class Community:
                 susceptible.append(person)
 
             if person.dest != None:
-                pygame.draw.line(self.surf, (0,255,255), person.coords, person.dest) 
+                pygame.draw.line(self.surf, theme['route'], person.coords, person.dest) 
 
         # zombie refering to infected person 
         for zombie in infected: 
@@ -882,10 +885,15 @@ class Menu(tk.Tk):
 
         self.geometry = f'{self.app_vars["sim_size"][0]}x{self.app_vars["sim_size"][1]}'
 
+        self.column0 = tk.Frame(self)
+        self.column1 = tk.Frame(self)
+
+        self.column0.grid(row=1,column=0, sticky=tk.N)
+        self.column1.grid(row=1,column=1, sticky=tk.N)
 
         # Simulation theme chooser
         self.theme_frame = ttk.LabelFrame(self, text='Simulation Theme')
-        self.theme_frame.pack(padx = 10, pady=10)
+        self.theme_frame.grid(row=0, column=0, padx = 10, pady = 10, columnspan= 2)
         self.theme_var = tk.StringVar()
         self.theme_var.set("dark")
         
@@ -895,7 +903,7 @@ class Menu(tk.Tk):
             button.grid(column=col, row=0, padx=5)
 
 
-        self.pathogen_frame = ttk.LabelFrame(self, text = 'Pathogen') 
+        self.pathogen_frame = ttk.LabelFrame(self.column0, text = 'Pathogen') 
         self.pathogen_frame.pack(padx = 5, pady = 5)
 
 
@@ -964,12 +972,12 @@ class Menu(tk.Tk):
             button.grid(column=col, row=0, padx=5)
 
 
-        self.mitigation_frame = ttk.LabelFrame(self, text = 'Mitigations') 
-        self.mitigation_frame.pack(padx = 5, pady = 5)
+        self.mitigation_frame = ttk.LabelFrame(self.column0, text = 'Mitigations') 
+        self.mitigation_frame.pack(padx = 5, pady= 5)
 
         # Migration chance chooser
         self.migrations_frame = ttk.LabelFrame(self.mitigation_frame, text='Migrations')
-        self.migrations_frame.grid(row = 0, column = 0, padx = 5, pady = 5)
+        self.migrations_frame.grid(row= 0, column= 0, padx= 5, pady= 5)
         self.migrations_var = tk.StringVar()
         self.migrations_values = {'Low': 0.01, 'Medium': 0.1, 'High': 0.25}
         self.migrations_var.set(self.migrations_values['Medium'])
@@ -985,7 +993,7 @@ class Menu(tk.Tk):
 
         # Move chance chance chooser
         self.movements_frame = ttk.LabelFrame(self.mitigation_frame, text='Movement Chance')
-        self.movements_frame.grid(row = 1, column = 0, padx = 5, pady = 5)
+        self.movements_frame.grid(row= 1, column= 0, padx= 5, pady= 5)
         self.movements_var = tk.StringVar()
         self.movements_values = {'Low': 0.01, 'Medium': 0.05, 'High': 0.15}
         self.movements_var.set(self.movements_values['Medium'])
@@ -998,26 +1006,101 @@ class Menu(tk.Tk):
                 variable=self.movements_var)
             button.grid(column=col, row=0, padx=5)
 
+        self.population_frame = ttk.LabelFrame(self.column1, text = 'Population Size')
+        self.population_frame.pack(side= tk.TOP, padx= 5, pady= 5)
+
+        self.population_slider = ttk.Scale(self.population_frame, from_ = 1, to = 2000, orient = 'horizontal', command=lambda _ :self.set_population_label())
+        self.population_slider.set(1000)
+        self.population_slider.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+        self.population_label = ttk.Label(self.population_frame, text = round(self.population_slider.get())) 
+        self.population_label.grid(row = 0, column = 1)
+
+
+        self.layout_frame = ttk.LabelFrame(self.column1, text = 'Community Layout')
+        self.layout_frame.pack(side= tk.TOP, padx = 5, pady = 10)
+
+        self.row_frame = ttk.LabelFrame(self.layout_frame, text = 'Rows')
+        self.row_frame.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+        self.row_slider = ttk.Scale(self.row_frame, from_ = 1, to = 8, orient = 'horizontal', command=lambda _ :self.set_row_label())
+        self.row_slider.set(1)
+        self.row_slider.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+        self.row_label = ttk.Label(self.row_frame, text = round(self.row_slider.get()))
+        self.row_label.grid(row = 0, column = 1)
+
+        self.col_frame = ttk.LabelFrame(self.layout_frame, text = 'Columns')
+        self.col_frame.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+        self.col_slider = ttk.Scale(self.col_frame, from_ = 1, to = 8, orient = 'horizontal', command=lambda _ :self.set_column_label())
+        self.col_slider.set(1)
+        self.col_slider.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+        self.col_label = ttk.Label(self.col_frame, text = round(self.col_slider.get()))
+        self.col_label.grid(row = 0, column = 1)
 
         self.save_button = ttk.Button(self, text='Save', command=lambda:self.save())
-        self.save_button.pack(pady = 5)
-        
-        self.exit_button = ttk.Button(self, text='Exit', command=lambda:self.exit())
-        self.exit_button.pack()
+        self.save_button.grid(row = 2, column = 0, pady = 5)
 
+        self.exit_button = ttk.Button(self, text='Exit', command=lambda:self.exit())
+        self.exit_button.grid(row = 2, column = 1)
+
+
+    def set_row_label(self):
+
+        self.row_label = ttk.Label(self.row_frame, text = round(self.row_slider.get())) 
+        self.row_label.grid(row = 0, column = 1)
+
+
+    def set_column_label(self):  
+
+        self.col_label = ttk.Label(self.col_frame, text = round(self.col_slider.get())) 
+        self.col_label.grid(row = 0, column = 1)
+
+    def set_population_label(self):  
+
+        self.population_label = ttk.Label(self.population_frame, text = round(self.population_slider.get())) 
+        self.population_label.grid(row = 0, column = 1)
 
 
     def save(self):
 
         self.config['app']['theme'] = str(self.theme_var.get())
+
         self.config['pathogen']['lethality'] = float(self.lethality_var.get())
         self.config['pathogen']['curability'] = float(self.curability_var.get())
         self.config['pathogen']['catchment'] = float(self.catchment_var.get())
         self.config['pathogen']['catchment'] = float(self.catchment_var.get())
 
         self.config['simulation']['migration_chance'] = float(self.migrations_var.get())
+        self.config['simulation']['move_chance'] = float(self.movements_var.get())
 
-        self.config['simulation']['migration_chance'] = float(self.movements_var.get())
+        population_size = round(self.population_slider.get())
+        rows = round(self.row_slider.get())
+        cols = round(self.col_slider.get())
+
+        community_size = population_size // (rows * cols) 
+        remainder = population_size  % (rows * cols) 
+
+        layout = []
+        places_choice =  [0, 1, 2, 3]
+        places_weights = [0.4, 0.4, 0.15, 0.05] 
+
+        for row in range(rows):
+            row_layout = [] 
+            for community in range(cols):
+
+                num_places = np.random.choice(places_choice, p=places_weights)
+                row_layout.append([int(community_size), int(num_places)])
+
+            layout.append(row_layout)
+
+        layout[0][0][0] += remainder 
+
+        self.config['simulation']['community_layout'] = layout
+        self.config['simulation']['total_population'] = population_size
+        self.config['simulation']['susceptible'] = population_size
 
         with open(self.config_addr, 'w') as config_file: 
             config_file.write(json.dumps(self.config))
