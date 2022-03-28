b5f16a49b33a97c3df22975c0ecc383517a9d01d
fontle <68354631+fontle@users.noreply.github.com>
2022-03-28 23:21:55 +0100

    Configuration now loads last saved defaults.

--- config.py
+++ config.py
@@ -6,17 +6,14 @@ from dataclasses import dataclass
 
 
 class _RadioFrame(ttk.LabelFrame):
-    def __init__(self, root, title: str, options: dict) -> None:
+    def __init__(self, root, title: str, options: dict, default) -> None:
         super().__init__(root, text=title)
 
         self.options = options
+        self.title = title
 
         self.var = tk.StringVar()
-        if len(self.options.keys()) > 2:
-            middle = len(self.options.keys()) // 2
-        else:
-            middle = 1
-        self.var.set(list(self.options.values())[middle])
+        self.var.set(default)
 
         for col, val in enumerate(self.options.keys()):
             button = ttk.Radiobutton(
@@ -28,6 +25,13 @@ class _RadioFrame(ttk.LabelFrame):
         """
         Returns the current toggled button
         """
+
+
+        #print()
+        #print(self.title)
+        #print("Var: ", self.var.get())
+        #print("Options: ", self.options)
+        #print('+++++++++++++++++++++++++++')
         return self.var.get()
 
 
@@ -72,12 +76,7 @@ class _Menu(tk.Tk):
         with open(self.config_addr, "r") as config_file:
             self.config = json.loads(config_file.read())
 
-        self.theme = self.config["theme"][self.config["app"]["theme"]]
-        self.sim_vars = self.config["simulation"]
-        self.app_vars = self.config["app"]
-        self.path_vars = self.config["pathogen"]
-
-        self.geometry = f'{self.app_vars["sim_size"][0]}x{self.app_vars["sim_size"][1]}'
+        self.geometry = f'{self.config["app"]["sim_size"][0]}x{self.config["app"]["sim_size"][1]}'
 
         self.column0 = tk.Frame(self)
         self.column1 = tk.Frame(self)
@@ -86,7 +85,10 @@ class _Menu(tk.Tk):
         self.column1.grid(row=1, column=1, sticky=tk.N)
 
         self.theme_chooser = _RadioFrame(
-            self, "Simulation Theme", {"Light": "light", "Dark": "dark"}
+            self,
+            "Simulation Theme",
+            {"Light": "light", "Dark": "dark"},
+            self.config["app"]["theme"]
         )
         self.theme_chooser.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 
@@ -97,6 +99,7 @@ class _Menu(tk.Tk):
             self.pathogen_frame,
             "Lethality",
             {"Low": "0.001", "Medium": "0.002", "High": "0.005"},
+            self.config["pathogen"]["lethality"]
         )
         self.lethality.grid(row=0, column=0, pady=5)
 
@@ -104,11 +107,15 @@ class _Menu(tk.Tk):
             self.pathogen_frame,
             "Curability",
             {"Low": "0.001", "Medium": "0.0025", "High": "0.005"},
+            self.config["pathogen"]["lethality"]
         )
         self.curability.grid(row=1, column=0, pady=5)
 
         self.catchment = _RadioFrame(
-            self.pathogen_frame, "Catchment", {"Low": "1", "Medium": "5", "High": "10"}
+            self.pathogen_frame,
+            "Catchment",
+            {"Low": "1", "Medium": "5", "High": "10"},
+            self.config["pathogen"]["catchment"]
         )
         self.catchment.grid(row=2, column=0, pady=5)
 
@@ -116,6 +123,7 @@ class _Menu(tk.Tk):
             self.pathogen_frame,
             "Infectiousness",
             {"Low": "0.03", "Medium": "0.1", "High": "0.25"},
+            self.config["pathogen"]["infectiousness"]
         )
         self.infectiousness.grid(row=3, column=0, pady=5)
 
@@ -126,6 +134,7 @@ class _Menu(tk.Tk):
             self.mitigation_frame,
             "Migrations",
             {"Low": "0.01", "Medium": "0.05", "High": "0.1"},
+            self.config["simulation"]["migrations"]
         )
         self.migrations.grid(row=0, column=0, padx=5, pady=5)
 
@@ -133,28 +142,34 @@ class _Menu(tk.Tk):
             self.mitigation_frame,
             "Movements",
             {"Low": "0.01", "Medium": "0.05", "High": "0.1"},
+            self.config["simulation"]["movements"]
         )
         self.movements.grid(row=1, column=0, padx=5, pady=5)
 
-        self.population = _Slider(self.column1, "Population Size", 1, 2000, 1000)
+        self.population = _Slider(self.column1, "Population Size", 1, 2000,
+                self.config["simulation"]["population"])
         self.population.pack(side=tk.TOP, padx=5, pady=5)
 
         self.layout_frame = ttk.LabelFrame(self.column1, text="Community Layout")
         self.layout_frame.pack(side=tk.TOP, padx=5, pady=10)
 
-        self.rows = _Slider(self.layout_frame, "Rows", 1, 8, 1)
+        self.rows = _Slider(self.layout_frame, "Rows", 1, 8,
+                len(self.config["simulation"]["layout"]))
         self.rows.grid(row=0, column=0, padx=5, pady=5)
 
-        self.cols = _Slider(self.layout_frame, "Columns", 1, 8, 1)
+        self.cols = _Slider(self.layout_frame, "Columns", 1, 8,
+                len(self.config["simulation"]["layout"][0]))
         self.cols.grid(row=1, column=0, padx=5, pady=5)
 
         self.size_frame = ttk.LabelFrame(self.column1, text="Simulation Size")
         self.size_frame.pack(side=tk.TOP, padx=5, pady=0)
 
-        self.width = _Slider(self.size_frame, "Width", 360, 1000, 600)
+        self.width = _Slider(self.size_frame, "Width", 360, 1000,
+                self.config["app"]["sim_size"][0])
         self.width.grid(row=0, column=0)
 
-        self.height = _Slider(self.size_frame, "Height", 360, 600, 400)
+        self.height = _Slider(self.size_frame, "Height", 360, 600,
+                self.config["app"]["sim_size"][1])
         self.height.grid(row=1, column=0, padx=5, pady=5)
 
         self.button_frame = ttk.Frame(self.column0)
@@ -173,13 +188,13 @@ class _Menu(tk.Tk):
     def save(self):
 
         self.config["app"]["theme"] = str(self.theme_chooser.fetch())
-        self.path_vars["lethality"] = float(self.lethality.fetch())
-        self.path_vars["curability"] = float(self.curability.fetch())
-        self.path_vars["catchment"] = float(self.catchment.fetch())
-        self.path_vars["infectiousness"] = float(self.infectiousness.fetch())
+        self.config["pathogen"]["lethality"] = float(self.lethality.fetch())
+        self.config["pathogen"]["curability"] = float(self.curability.fetch())
+        self.config["pathogen"]["catchment"] = int(self.catchment.fetch())
+        self.config["pathogen"]["infectiousness"] = float(self.infectiousness.fetch())
 
-        self.sim_vars["migration"] = float(self.migrations.fetch())
-        self.sim_vars["movement"] = float(self.movements.fetch())
+        self.config["simulation"]["migrations"] = float(self.migrations.fetch())
+        self.config["simulation"]["movements"] = float(self.movements.fetch())
 
         population_size = self.population.fetch()
         rows = self.rows.fetch()
@@ -205,14 +220,17 @@ class _Menu(tk.Tk):
         # Add excess population to first community
         layout[0][0][0] += remainder
 
-        self.sim_vars["layout"] = layout
-        self.sim_vars["population"] = population_size
-        self.sim_vars["susceptible"] = population_size
+        self.config["simulation"]["layout"] = layout
+        self.config["simulation"]["population"] = population_size
+        self.config["simulation"]["susceptible"] = population_size
 
         # Round width and height to nearest 10 to prevent weird remainders
         width = round(self.width.fetch() / 10) * 10
         height = round(self.height.fetch() / 10) * 10
-        self.app_vars["sim_size"] = width, height
+        self.config["app"]["sim_size"] = [width, height]
+
+        with open(self.config_addr, "w") as config_file:
+            config_file.write(json.dumps(self.config))
 
     def exit(self):
         self.save()
@@ -263,14 +281,14 @@ class _Pathogen:
 menu = _Menu()
 menu.mainloop()
 
-theme = _Theme(*menu.theme.values())
-sim = _Sim(*menu.sim_vars.values())
-app = _App(*menu.app_vars.values())
-pathogen = _Pathogen(*menu.path_vars.values())
-del menu
+sim = _Sim(*menu.config["simulation"].values())
+app = _App(*menu.config["app"].values())
+theme = _Theme(*menu.config["theme"][menu.config["app"]["theme"]].values())
+pathogen = _Pathogen(*menu.config["pathogen"].values())
 
 if __name__ == "__main__":
-    print(theme.__dict__)
+
     print(sim.__dict__)
     print(app.__dict__)
+    print(theme.__dict__)
     print(pathogen.__dict__)
